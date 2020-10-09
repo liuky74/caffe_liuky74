3d53c1297100574d70d5ed77c9a62c2510fcbf1f
liuky74 <233941413@qq.com>
2020-10-09 18:42:15 +0800

    添加了base conv layer的注释

--- src/caffe/layers/base_conv_layer.cpp
+++ src/caffe/layers/base_conv_layer.cpp
@@ -13,17 +13,18 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   // Configure the kernel size, padding, stride, and inputs.
   ConvolutionParameter conv_param = this->layer_param_.convolution_param();
-  force_nd_im2col_ = conv_param.force_nd_im2col();
-  channel_axis_ = bottom[0]->CanonicalAxisIndex(conv_param.axis());
-  const int first_spatial_axis = channel_axis_ + 1;
-  const int num_axes = bottom[0]->num_axes();
-  num_spatial_axes_ = num_axes - first_spatial_axis;
-  CHECK_GE(num_spatial_axes_, 0);
+  force_nd_im2col_ = conv_param.force_nd_im2col();/*是否强制将图像按照卷积核大小进行列化(转换为caffe计算矩阵)*/
+  channel_axis_ = bottom[0]->CanonicalAxisIndex(conv_param.axis());/*channel所在的axis*/
+  const int first_spatial_axis = channel_axis_ + 1;/*图像矩阵的空间dim起始位置，即channel * row * colum中row所在的axis*/
+  const int num_axes = bottom[0]->num_axes();/*传入数据的dim数量，注意dim与shape之间的不同*/
+  num_spatial_axes_ = num_axes - first_spatial_axis;/*得到传入数据的shape中表述空间的dim有几个*/
+  CHECK_GE(num_spatial_axes_, 0);/*检查传入数据的shape是否至少包含1个空间dim，即是否至少包含row*/
   vector<int> bottom_dim_blob_shape(1, num_spatial_axes_ + 1);
-  vector<int> spatial_dim_blob_shape(1, std::max(num_spatial_axes_, 1));
+  vector<int> spatial_dim_blob_shape(1, std::max(num_spatial_axes_, 1));/*存储传入图像的row colum值*/
   // Setup filter kernel dimensions (kernel_shape_).
-  kernel_shape_.Reshape(spatial_dim_blob_shape);
-  int* kernel_shape_data = kernel_shape_.mutable_cpu_data();
+  kernel_shape_.Reshape(spatial_dim_blob_shape);/*根据传入数据的row colum数据初始化卷积核的dim数据，从而区分是几维的卷积*/
+  int* kernel_shape_data = kernel_shape_.mutable_cpu_data();/*获取卷积核shape的数据指针*/
+  /*开始从param中读取参数，对卷积核的shape进行赋值*/
   if (conv_param.has_kernel_h() || conv_param.has_kernel_w()) {
     CHECK_EQ(num_spatial_axes_, 2)
         << "kernel_h & kernel_w can only be used for 2D convolution.";
@@ -42,12 +43,14 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
             conv_param.kernel_size((num_kernel_dims == 1) ? 0 : i);
       }
   }
+  /*检查卷积核各个dim中的值是否大于0，即正常数据[batch_size,channel,row,colum]中任何一个参数都不应为0*/
   for (int i = 0; i < num_spatial_axes_; ++i) {
     CHECK_GT(kernel_shape_data[i], 0) << "Filter dimensions must be nonzero.";
   }
   // Setup stride dimensions (stride_).
-  stride_.Reshape(spatial_dim_blob_shape);
-  int* stride_data = stride_.mutable_cpu_data();
+  stride_.Reshape(spatial_dim_blob_shape);/*记录步长的shape dim数量同样应与输入数据的空间dim相等*/
+  int* stride_data = stride_.mutable_cpu_data();/*取出数据指针*/
+  /*根据传入的参数对stride进行赋值*/
   if (conv_param.has_stride_h() || conv_param.has_stride_w()) {
     CHECK_EQ(num_spatial_axes_, 2)
         << "stride_h & stride_w can only be used for 2D convolution.";
@@ -70,7 +73,7 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     }
   }
   // Setup pad dimensions (pad_).
-  pad_.Reshape(spatial_dim_blob_shape);
+  pad_.Reshape(spatial_dim_blob_shape);/*逻辑同上，开始设置pad参数*/
   int* pad_data = pad_.mutable_cpu_data();
   if (conv_param.has_pad_h() || conv_param.has_pad_w()) {
     CHECK_EQ(num_spatial_axes_, 2)
@@ -93,7 +96,7 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     }
   }
   // Setup dilation dimensions (dilation_).
-  dilation_.Reshape(spatial_dim_blob_shape);
+  dilation_.Reshape(spatial_dim_blob_shape);/*设置膨胀参数*/
   int* dilation_data = dilation_.mutable_cpu_data();
   const int num_dilation_dims = conv_param.dilation_size();
   CHECK(num_dilation_dims == 0 || num_dilation_dims == 1 ||
@@ -108,21 +111,21 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   }
   // Special case: im2col is the identity for 1x1 convolution with stride 1
   // and no padding, so flag for skipping the buffer and transformation.
+  /*判断卷积核是不是1x1的，如果是1x1的那么换算成caffe计算矩阵会简略很多*/
   is_1x1_ = true;
   for (int i = 0; i < num_spatial_axes_; ++i) {
-    is_1x1_ &=
-        kernel_shape_data[i] == 1 && stride_data[i] == 1 && pad_data[i] == 0;
+    is_1x1_ &= kernel_shape_data[i] == 1 && stride_data[i] == 1 && pad_data[i] == 0;/*与操作判断卷积核的shape都为1且pad都为0*/
     if (!is_1x1_) { break; }
   }
   // Configure output channels and groups.
-  channels_ = bottom[0]->shape(channel_axis_);
-  num_output_ = this->layer_param_.convolution_param().num_output();
+  channels_ = bottom[0]->shape(channel_axis_);/*获取bottom的channel数量*/
+  num_output_ = this->layer_param_.convolution_param().num_output();/*输出的channel数量*/
   CHECK_GT(num_output_, 0);
-  group_ = this->layer_param_.convolution_param().group();
-  CHECK_EQ(channels_ % group_, 0);
+  group_ = this->layer_param_.convolution_param().group();/*是否有分组卷积*/
+  CHECK_EQ(channels_ % group_, 0);/*分组是均分的，所以必须要能够整除*/
   CHECK_EQ(num_output_ % group_, 0)
       << "Number of output should be multiples of group.";
-  if (reverse_dimensions()) {
+  if (reverse_dimensions()) {/*是否要颠倒卷积的in_channel和out_channel，因为反卷积时的输出和输入参数与正向卷积刚好是相反的*/
     conv_out_channels_ = channels_;
     conv_in_channels_ = num_output_;
   } else {
@@ -132,18 +135,19 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   // Handle the parameters: weights and biases.
   // - blobs_[0] holds the filter weights
   // - blobs_[1] holds the biases (optional)
-  vector<int> weight_shape(2);
+  vector<int> weight_shape(2);/*kenel权重的shape，假设in_ch=3,out_ch=64，kernel size=5则有weight shape：[64,3,5,5]*/
   weight_shape[0] = conv_out_channels_;
   weight_shape[1] = conv_in_channels_ / group_;
+
   for (int i = 0; i < num_spatial_axes_; ++i) {
     weight_shape.push_back(kernel_shape_data[i]);
   }
-  bias_term_ = this->layer_param_.convolution_param().bias_term();
-  vector<int> bias_shape(bias_term_, num_output_);
-  if (this->blobs_.size() > 0) {
+  bias_term_ = this->layer_param_.convolution_param().bias_term();/*标识是否使用bias*/
+  vector<int> bias_shape(bias_term_,num_output_);/*声明一个bias_shape,注意这里bias_term_是自动将bool转为了0/1*/
+  if (this->blobs_.size() > 0) {/*在conv层中有一个blobs数组，默认blobs[0]用于保存kenel的权重weight，blobs[1]保存偏置bias*/
     CHECK_EQ(1 + bias_term_, this->blobs_.size())
         << "Incorrect number of weight blobs.";
-    if (weight_shape != this->blobs_[0]->shape()) {
+    if (weight_shape != this->blobs_[0]->shape()) {/*判断weight的shape形状是否和上文中设置的一致*/
       Blob<Dtype> weight_shaped_blob(weight_shape);
       LOG(FATAL) << "Incorrect weight shape: expected shape "
           << weight_shaped_blob.shape_string() << "; instead, shape was "
@@ -164,10 +168,12 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     }
     // Initialize and fill the weights:
     // output channels x input channels per-group x kernel height x kernel width
+    /*上文都是关于超参数的判断，这里开始初始化*/
     this->blobs_[0].reset(new Blob<Dtype>(weight_shape));
+    /*定义一个初始化滤波器，根据参数的不同（比如bilinear）对weight值进行初始化*/
     shared_ptr<Filler<Dtype> > weight_filler(GetFiller<Dtype>(
         this->layer_param_.convolution_param().weight_filler()));
-    weight_filler->Fill(this->blobs_[0].get());
+    weight_filler->Fill(this->blobs_[0].get());/*根据blob[0]初始化的值进行填充*/
     // If necessary, initialize and fill the biases.
     if (bias_term_) {
       this->blobs_[1].reset(new Blob<Dtype>(bias_shape));
@@ -176,51 +182,58 @@ void BaseConvolutionLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       bias_filler->Fill(this->blobs_[1].get());
     }
   }
-  kernel_dim_ = this->blobs_[0]->count(1);
-  weight_offset_ = conv_out_channels_ * kernel_dim_ / group_;
+  kernel_dim_ = this->blobs_[0]->count(1);/*计算一个完整的kernel的数据量，比如weight shape：[64,3,5,5]，则一个完整的kernel 数据量为3x5x5*/
+  weight_offset_ = conv_out_channels_ * kernel_dim_ / group_;/*分组权重的指针偏移量，如果需要分组卷积，则需要得到每一组的权重起始位置*/
   // Propagate gradients to the parameters (as directed by backward pass).
-  this->param_propagate_down_.resize(this->blobs_.size(), true);
+  this->param_propagate_down_.resize(this->blobs_.size(), true);/*这个参数决定了各个blob是否需要计算反向传播*/
 }
 
 template <typename Dtype>
 void BaseConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   const int first_spatial_axis = channel_axis_ + 1;
-  CHECK_EQ(bottom[0]->num_axes(), first_spatial_axis + num_spatial_axes_)
+  CHECK_EQ(bottom[0]->num_axes(), first_spatial_axis + num_spatial_axes_)/*检查bottom的输入dim*/
       << "bottom num_axes may not change.";
   num_ = bottom[0]->count(0, channel_axis_);
-  CHECK_EQ(bottom[0]->shape(channel_axis_), channels_)
+  CHECK_EQ(bottom[0]->shape(channel_axis_), channels_)/*检查bottom的channel*/
       << "Input size incompatible with convolution kernel.";
   // TODO: generalize to handle inputs of different shapes.
-  for (int bottom_id = 1; bottom_id < bottom.size(); ++bottom_id) {
+  for (int bottom_id = 1; bottom_id < bottom.size(); ++bottom_id) {/*比对每个bottom，确保shape相等*/
     CHECK(bottom[0]->shape() == bottom[bottom_id]->shape())
         << "All inputs must have the same shape.";
   }
   // Shape the tops.
+  /*根据参数设置top的shape*/
   bottom_shape_ = &bottom[0]->shape();
-  compute_output_shape();
+  compute_output_shape();/*计算输出的shape，虚函数，由各个子函数重写*/
+  /*这里利用指针获取了shape中CxHxW之前的所有数据，即BatchSize*/
   vector<int> top_shape(bottom[0]->shape().begin(),
       bottom[0]->shape().begin() + channel_axis_);
-  top_shape.push_back(num_output_);
+  top_shape.push_back(num_output_);/*添加输出channel*/
   for (int i = 0; i < num_spatial_axes_; ++i) {
-    top_shape.push_back(output_shape_[i]);
+    top_shape.push_back(output_shape_[i]);/*添加输出的空间形状，output_shape_会在各个子类中计算得到*/
   }
   for (int top_id = 0; top_id < top.size(); ++top_id) {
-    top[top_id]->Reshape(top_shape);
+    top[top_id]->Reshape(top_shape);/*将top进行reshape*/
   }
+  /*如果是反卷积，则输出的dim总数应从bottom中取*/
   if (reverse_dimensions()) {
     conv_out_spatial_dim_ = bottom[0]->count(first_spatial_axis);
   } else {
     conv_out_spatial_dim_ = top[0]->count(first_spatial_axis);
   }
+    /* 转变为caffe计算矩阵后，col_offset_即每个完整的卷积核按照参数卷积整个输入图像所需的计算量，
+     * 在img2col操作后，每一个卷积核都展开reshape为一行以进行矩阵运算*/
   col_offset_ = kernel_dim_ * conv_out_spatial_dim_;
-  output_offset_ = conv_out_channels_ * conv_out_spatial_dim_ / group_;
+  output_offset_ = conv_out_channels_ * conv_out_spatial_dim_ / group_;/*分组卷积的情况下，得到不同分组的输出数据偏移*/
   // Setup input dimensions (conv_input_shape_).
-  vector<int> bottom_dim_blob_shape(1, num_spatial_axes_ + 1);
+  /*设置卷积输出的shape大小*/
+  vector<int> bottom_dim_blob_shape(1, num_spatial_axes_ + 1);/*num_spatial_axes_+1是在空间维度信息的基础上添加了channel*/
   conv_input_shape_.Reshape(bottom_dim_blob_shape);
   int* conv_input_shape_data = conv_input_shape_.mutable_cpu_data();
+  /*设置具体的conv_input_shape值*/
   for (int i = 0; i < num_spatial_axes_ + 1; ++i) {
-    if (reverse_dimensions()) {
+    if (reverse_dimensions()) {/*反卷积的情况下要top才是输入*/
       conv_input_shape_data[i] = top[0]->shape(channel_axis_ + i);
     } else {
       conv_input_shape_data[i] = bottom[0]->shape(channel_axis_ + i);
@@ -229,9 +242,9 @@ void BaseConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   // The im2col result buffer will only hold one image at a time to avoid
   // overly large memory usage. In the special case of 1x1 convolution
   // it goes lazily unused to save memory.
-  col_buffer_shape_.clear();
+  col_buffer_shape_.clear();/*用于保存col化的输入图像的shape，注意是shape*/
   col_buffer_shape_.push_back(kernel_dim_ * group_);
-  for (int i = 0; i < num_spatial_axes_; ++i) {
+  for (int i = 0; i < num_spatial_axes_; ++i) {/*最终得到[kernel_dim*group,row,colum]*/
     if (reverse_dimensions()) {
       col_buffer_shape_.push_back(input_shape(i + 1));
     } else {
@@ -239,14 +252,14 @@ void BaseConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
     }
   }
   col_buffer_.Reshape(col_buffer_shape_);
-  bottom_dim_ = bottom[0]->count(channel_axis_);
-  top_dim_ = top[0]->count(channel_axis_);
-  num_kernels_im2col_ = conv_in_channels_ * conv_out_spatial_dim_;
-  num_kernels_col2im_ = reverse_dimensions() ? top_dim_ : bottom_dim_;
+  bottom_dim_ = bottom[0]->count(channel_axis_);/*获得输入的dim总数（CHW）*/
+  top_dim_ = top[0]->count(channel_axis_);/*获得输出的dim总数（CHW）*/
+  num_kernels_im2col_ = conv_in_channels_ * conv_out_spatial_dim_;/*输入的channel * 输出的空间维度部分的数据总量（即row*colum），im2col_nd_gpu函数才会用到*/
+  num_kernels_col2im_ = reverse_dimensions() ? top_dim_ : bottom_dim_;/*同上im2col_nd_gpu函数才会用到*/
   // Set up the all ones "bias multiplier" for adding biases by BLAS
-  out_spatial_dim_ = top[0]->count(first_spatial_axis);
-  if (bias_term_) {
-    vector<int> bias_multiplier_shape(1, out_spatial_dim_);
+  out_spatial_dim_ = top[0]->count(first_spatial_axis);/*输出的空前维度部分的数据总量*/
+  if (bias_term_) {/*如果使用bias则设置bias*/
+    vector<int> bias_multiplier_shape(1, out_spatial_dim_);/*bias的一个倍数矩阵，应该是bias的一个权重*/
     bias_multiplier_.Reshape(bias_multiplier_shape);
     caffe_set(bias_multiplier_.count(), Dtype(1),
         bias_multiplier_.mutable_cpu_data());
